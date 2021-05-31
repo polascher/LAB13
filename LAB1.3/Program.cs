cb524855878fa21264e2e64fc28ae719816981b2
polascher <polascher@gmail.com>
2021-05-31 18:34:50 +0700

    LAB13

---
+++ LAB1.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LAB13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            string n1 = Console.ReadLine();
+            double a = Convert.ToDouble(n1);
+            Console.WriteLine("Введите второе число:");
+            string n2 = Console.ReadLine();
+            double b = Convert.ToDouble(n2);
+            double x = a;
+            a = b;
+            b = x;
+            Console.WriteLine("Первое число = {0}, Второе число = {1}", a, b);
+        }
+    }
+}
